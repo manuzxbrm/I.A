a921caaea553e94581724829b0bb504b128d1ea5
manuzxbrm <148786839+manuzxbrm@users.noreply.github.com>
2025-06-18 18:42:40 -0300

    f

--- script.js
+++ script.js
@@ -75,6 +75,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
@@ -86,12 +87,15 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativa = document.createElement("button");
         botaoAlternativa.textContent = alternativa.texto;
-        botaoAlternativa.addEventListener("click", function() {
-            atual++;
-            mostraPergunta()
-        })
+        botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativa);
     }
 }
 
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes =  opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+            atual++;
+            mostraPergunta();
+}
 mostraPergunta();
